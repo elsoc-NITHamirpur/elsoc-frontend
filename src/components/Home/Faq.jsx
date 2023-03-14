040bb664c66a67b41a4bb45ff503f72a7b0f583c
Joiyad <20bee012@nith.ac.in>
2023-03-14 23:23:55 +0530

    FAQ questions updated

--- src/components/Home/Faq.jsx
+++ src/components/Home/Faq.jsx
@@ -26,19 +26,19 @@ const Faq = () => {
       <Accordion className="text-white border-teal-600 border p-2 md:p-4 cursor-pointer rounded-lg" open={open === 1} onClick={() => handleOpen(1)}>
         <AccordionHeader>What does ELSOC do ?</AccordionHeader>
         <AccordionBody>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus dicta incidunt voluptatem temporibus, dolorem iure distinctio suscipit aut, voluptates deleniti dolor, nostrum expedita fuga! Quam, nostrum facilis. Tempore, deleniti tenetur?
+          ELSOC is the society of Electrical engineering which organizes various events and workshops
         </AccordionBody>
       </Accordion>
       <Accordion className="text-white border-teal-600 border p-2 md:p-4 my-8 cursor-pointer rounded-lg" open={open === 2} onClick={() => handleOpen(2)}>
         <AccordionHeader>What are the events organized by the ELSOC throughout the year ?</AccordionHeader>
         <AccordionBody>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus non eaque cupiditate nihil accusamus, assumenda delectus magnam itaque iusto explicabo debitis. Animi optio iure ipsa non tempora molestias doloribus ex?
+          Viva questionnaire, Intern Talk , Workshop on Blockchain technology are some of the events organized by ELSOC.
         </AccordionBody>
       </Accordion>
       <Accordion className="text-white border-teal-600 border p-2 md:p-4 cursor-pointer rounded-lg" open={open === 3} onClick={() => handleOpen(3)}>
         <AccordionHeader>How to join ELSOC ? What is the procedure for this ?</AccordionHeader>
         <AccordionBody>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui esse odit illum iste ipsum repudiandae ex dignissimos reprehenderit tempore dolorum eaque aperiam accusantium, itaque recusandae asperiores laudantium, fuga enim natus!
+          You can join ELSOC in your 1st or 2nd year by going through interview process.
         </AccordionBody>
       </Accordion>
     </Fragment>
